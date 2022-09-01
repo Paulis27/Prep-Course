f3a7923212b0bd267473760c7c73998e2a6fff37
Paulis27 <paulis27288@gmail.com>
2022-09-01 14:41:17 -0300

    Mod numero

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'cielo';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = '27';
 
 // Crea una variable booleana:
 const nuevoBool = null;
